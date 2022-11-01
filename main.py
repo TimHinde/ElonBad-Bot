de23cc67a8f2751f0b683b0af709c9a76b120922
Tim <t.c.hinde@gmail.com>
2022-11-01 21:37:31 +0000

    Created adjective files for insult generation - lists need review

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+
+Abject
+Abnormal
+Abrasive
+Abrupt
+Absent
+Absentminded
+Absurd
+Abusive
+Abysmal
+Accidental
+Acerbic
+Aching	Acrimonious
+Adversarial
+Afraid
+Aggravating
+Aggressive
+Agonizing
+Aimless
+Alarming
+Aloof
+Anemic
+Angry
+Annoying
+Antagonistic
+
+Antiquated
+Appalling
+Apprehensive
+Argumentative
+Arrogant
+Artificial
+Artless
+Ashamed
+Astringent
+Atrocious
+Awful
+Awkward
+
+if __name__ == '__main':
+    pass
